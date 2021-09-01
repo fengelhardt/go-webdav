3dc11ca0af5ce5f01dc8ffab53fa3de1554abc8b
Frank Engelhardt <fengelha@ovgu.de>
2021-09-01 18:06:13 +0200

    auth: remove dead code

--- auth.go
+++ auth.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"crypto/md5"
 	"encoding/base64"
-	"encoding/hex"
 	"fmt"
 	"strings"
 
@@ -44,19 +42,6 @@ func parseUser(propstr string) string {
 	return ""
 }
 
-func hash(s string) string {
-	h := md5.Sum([]byte(s))
-	return hex.EncodeToString(h[:])
-}
-
-func getNonce() string {
-	return "abcdef"
-}
-
-func unquote(s string) string {
-	return strings.Trim(s, "\"")
-}
-
 func trim(s string) string {
 	return strings.Trim(s, " \t\n\r")
 }
@@ -67,32 +52,4 @@ func cutPropertyString(s string) string {
 	} else {
 		return ""
 	}
-}
-
-func parsePropertyString(s string) map[string]string {
-	propmap := make(map[string]string)
-	props := strings.Split(s, ",")
-	for _, p := range props {
-		if p != "" {
-			kv := strings.Split(p, "=")
-			propmap[strings.ToLower(trim(kv[0]))] = trim(unquote(kv[1]))
-		}
-	}
-	return propmap
-}
-
-func checkPropertyMap(prop map[string]string) bool {
-	ret := true
-	var ok bool
-	_, ok = prop["username"]
-	ret = ret && ok
-	_, ok = prop["realm"]
-	ret = ret && ok
-	_, ok = prop["uri"]
-	ret = ret && ok
-	_, ok = prop["nonce"]
-	ret = ret && ok
-	_, ok = prop["response"]
-	ret = ret && ok
-	return ret
 }
